adb45a7a103492c1e87e187faa791f2c5b0a2f0d
arunrawat14 <arunrawat1410@gmail.com>
2023-07-21 14:03:43 +0530

    my first commit

--- src/App.js
+++ src/App.js
@@ -4,9 +4,9 @@ import reviews from './data';
 
 const App = () => {
   return (
-    <div className='flex flex-col justify-center items-center w-full h-screen bg-gray-200'>
+    <div className='flex flex-col justify-center items-center w-full h-screen bg-gray-200 min-h-full'>
       <div className='text-center flex flex-col justify-center items-center'>
-        <h1 className='text-4xl font-bold'>Our Testimonials</h1>
+        <h1 className='text-4xl font-bold '>Our Testimonials</h1>
         <div className='flex justify-center w-[100%] h-[10%]'>
           <div className='bg-violet-400 h-[0.3rem] w-[5rem] mt-7'></div>
         </div>
--- src/components/Card.js
+++ src/components/Card.js
@@ -1,47 +1,39 @@
-import React from 'react'
-import {FaQuoteLeft,FaQuoteRight} from 'react-icons/fa'
+import React from 'react';
+import { FaQuoteLeft, FaQuoteRight } from 'react-icons/fa';
 
 export default function Card(props) {
-
-    let review = props.review
+  const review = props.review;
 
   return (
-    <div className='flex flex-col md:relative   '>
-
-<div className='relative'>
+    <div className='flex flex-col md:relative'>
+      <div className='relative'>
         <img
           src={review.image}
           alt='images'
-          className='rounded-full w-[8rem] h-[8rem] md:w-[10rem] md:h-[10rem] top-[-75px] left-[10px] absolute z-10 aspect-square'
+          className='rounded-full w-[5rem] h-[5rem] md:w-[10rem] md:h-[10rem] top-[-70px] left-[-25px] md:top-[-5] md:left-[1] absolute z-10 aspect-square'
         />
-        <div className='bg-violet-400 w-[8rem] h-[8rem] md:w-[10rem] md:h-[10rem] rounded-full absolute left-[20px] top-[-75px] z-0'></div>
+        <div className='bg-violet-400 w-[5rem] h-[5rem] md:w-[10rem] md:h-[10rem] rounded-full absolute left-[-15px] top-[-70px] md:top-[-5] md:left-[1] z-0'></div>
       </div>
 
-        <div className='text-center mt-7'>
-            <p className='text-2xl font-bold capitalize '>{review.name}</p>
-        </div>
+      <div className='text-center mt-7'>
+        <p className='text-2xl font-bold capitalize'>{review.name}</p>
+      </div>
 
-        <div className='text-center mt-7'>
-            <p className=' text-violet-400 uppercase text-sm '>{review.job}</p>
-        </div>
+      <div className='text-center mt-7'>
+        <p className='text-violet-400 uppercase text-sm'>{review.job}</p>
+      </div>
 
-        <div className='text-violet-400 mx-auto mt-5' >
-            <FaQuoteLeft/>
-        </div>
+      <div className='text-violet-400 mx-auto mt-5'>
+        <FaQuoteLeft />
+      </div>
 
-        <div className='mt-5 text-slate-500 w-full md:w-[35vw] whitespace-pre-line text-justify  '>
-            {review.text}
-         
-        </div>
+      <div className='mt-5 text-slate-500 w-full md:w-[35vw] whitespace-pre-line text-justify'>
+        {review.text}
+      </div>
 
-        <div className='text-violet-400 mx-auto mt-5'>
-            <FaQuoteRight/>
-        </div>
-
-       
-
-
-
+      <div className='text-violet-400 mx-auto mt-5'>
+        <FaQuoteRight />
+      </div>
     </div>
-  )
+  );
 }
